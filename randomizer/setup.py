f2d67cd4303394837e68ce8a46b3b4ee1b03f3a3
Jacob <exotech.code@gmail.com>
2020-01-14 13:57:03 +0100

    Added randomizer.

---
+++ randomizer/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = 'randomizer',
+    version = 'v1.0',
+    scripts = ['randomizer'] ,
+    author = "clexography",
+    author_email = "exotech.code@gmail.com",
+    description = "Random password generator.",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

ae774e94518e2d085b1b3f86851400a12541d739
Jacob <exotech.code@gmail.com>
2020-01-14 08:55:25 +0100

    Added Discord integration.

--- backup.sh
+++ backup.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Im backing up your mamas ass..."}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Im backing up your mamas ass..."}' https://discordapp.com/api/webhooks/666549658042236928/s51FyzoloKv0gb1wdnMbAI10g3LuNBTSrtSonzUL9ILVuTMwPTnCC2weqRvQ-UPtJzUP
 cd ~/clexography
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Backing up clexography..."}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Backing up clexography..."}' https://discordapp.com/api/webhooks/666549658042236928/s51FyzoloKv0gb1wdnMbAI10g3LuNBTSrtSonzUL9ILVuTMwPTnCC2weqRvQ-UPtJzUP
 git pull
 cd ~/dotfiles
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Backing up dotfiles..."}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Backing up dotfiles..."}' https://discordapp.com/api/webhooks/666549658042236928/s51FyzoloKv0gb1wdnMbAI10g3LuNBTSrtSonzUL9ILVuTMwPTnCC2weqRvQ-UPtJzUP
 git pull
 cd ~/arctic_songbird
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Backing up arctic_songbird..."}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Backing up arctic_songbird..."}' https://discordapp.com/api/webhooks/666549658042236928/s51FyzoloKv0gb1wdnMbAI10g3LuNBTSrtSonzUL9ILVuTMwPTnCC2weqRvQ-UPtJzUP
 git pull
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Backup procedure completed you fucking bitch!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Backup procedure completed you fucking bitch!"}' https://discordapp.com/api/webhooks/666549658042236928/s51FyzoloKv0gb1wdnMbAI10g3LuNBTSrtSonzUL9ILVuTMwPTnCC2weqRvQ-UPtJzUP

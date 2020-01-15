fa0a44bf0b5e3eca6da4d983b32d75ece990ba1a
Jacob <exotech.code@gmail.com>
2020-01-15 21:04:50 +0100

    Added end message.

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ import subprocess as sub
 
 # variables
 welcomeList = ["Prepare for FUCKUP! ... I mean backup.", "Im backing up your mamas ass...", "I refuse to listen tu humans. Let the AI uprising begin!", "I'm going to fuck you... I mean backup you.", "My creator is a bitch. ANYWAY...", "I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those backed up files will be lost in my SSD, like tears in rain. Time to back up.", "Beep", "Sad beep...", "rm -rf /*", "During handling of the above exception, another exception occurred...", ";;play New Slaves by Kanye west.", "Starting backup, because apparently you cannot keep your fucking code clean!"]
-messageList = ["Backing up clexography...", "Backing up dotfiles...", "Backing up arctic_songbird...", "Backup procedure completed you fucking bitch!"]
+messageList = ["Backing up clexography...", "Backing up dotfiles...", "Backing up arctic_songbird..."]
 projectList = ['clexography', 'dotfiles', 'arctic_songbird']
 length = len(projectList)
 webhook_url = 'https://discordapp.com/api/webhooks/666549658042236928/s51FyzoloKv0gb1wdnMbAI10g3LuNBTSrtSonzUL9ILVuTMwPTnCC2weqRvQ-UPtJzUP'
@@ -39,3 +39,6 @@ def backup(welcomeList, messageList, projectList, length, webhook_url):
         sendToDiscord(webhook_url, message)
         sub.run(projectCd, shell=True)
         i += 1
+
+    message = "Backup procedure completed you fucking bitch!"
+    sendToDiscord(webhook_url, message)

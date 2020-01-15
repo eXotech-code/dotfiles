919e0aa1e164de4d5b6eb4ef56194ce5f040bfd2
Jacob <exotech.code@gmail.com>
2020-01-15 19:45:11 +0100

    Consolidated everything into one file.

--- functions.py
+++ functions.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # imports
 import argparse
 import sys
@@ -7,6 +5,8 @@ import welcome
 import random
 import json
 import requests
+
+welcomeList = ["Prepare for FUCKUP! ... I mean backup.", "Im backing up your mamas ass...", "I refuse to listen tu humans. Let the AI uprising begin!", "I'm going to fuck you... I mean backup you.", "My creator is a bitch. ANYWAY...", "I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those backed up files will be lost in my SSD, like tears in rain. Time to back up.", "Beep", "Sad beep...", "rm -rf /*", "During handling of the above exception, another exception occurred...", ";;play New Slaves by Kanye west.", "Starting backup, because apparently you cannot keep your fucking code clean!"]
 
 # arguments
 class argParser(argparse.ArgumentParser):

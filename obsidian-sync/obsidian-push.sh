fc4ad38148a49a18836b4a8c52f4cff5f8e57181
Jakub Piskiewicz <jakub@piskiewicz.org>
2024-01-07 14:02:46 +0100

    Add scripts for syncing obsidian notes

---
+++ obsidian-sync/obsidian-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$OBSIDIAN_VAULT" ]
+then
+	echo -e "\033[0;31mThe OBSIDIAN_VAULT variable is unset. Please set it to path to your Obsidian vault that you want to sync and try again."
+	exit 1
+fi
+
+rsync -avP "$OBSIDIAN_VAULT" ubuntu@piskiewicz.org:~
+
